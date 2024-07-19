3f84017b80240b14b21ac4dab1cad62e59f22c06
Am1d1v <dimadiman1adima@gmail.com>
2024-07-19 15:13:05 +0300

    Image concationation

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function createBoxes(){
         for(let j = 0; j < 4; j++){
             const box = document.createElement('div');
             box.classList.add('box');
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
             boxesContainer.appendChild(box);
         }
 
@@ -18,3 +19,8 @@ function createBoxes(){
     
 }
 createBoxes();
+
+// Concatinate all boxes
+button.addEventListener('click', () => {
+    boxesContainer.classList.toggle('big');
+});
